333a82b4fd4dd6f9b4aa7fbec2caf02da84f633f
katjalmarko <katjalmarko@gmail.com>
2023-04-26 23:46:04 +0200

    header vercel fix3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,10 @@
+import { Starwars } from '../ImageImports';
+
 const Header = () => {
   return (
     <div>
       <div className="flex flex-col items-center justify-center mt-10">
-        <img
-          src="/public/images/Starwars.png"
-          alt="logo"
-          className="h-24 w-auto"
-        />
+        <img src={Starwars} alt="logo" className="h-24 w-auto" />
         <p className="text-white text-4xl font-bold mt-4 animate-pulse font-wallpoet">
           CHARACTERS
         </p>
